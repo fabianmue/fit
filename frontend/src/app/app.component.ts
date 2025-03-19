6782d44cca294b5247c3a5b75e7edc09b51e0aba
Fabian Mueller <famulo@bluewin.ch>
2025-03-19 20:37:41 +0100

    feature: home

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -9,4 +10,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private readonly titleService: Title) {
+    this.titleService.setTitle('FIT Invest');
+  }
+}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 
+import { HomeComponent } from '@app/home/home.component';
 import { CompaniesDashboardComponent } from '@app/companies/companies-dashboard/companies-dashboard.component';
 import { CharacteristicsDashboardComponent } from '@app/characteristics/characteristics-dashboard/characteristics-dashboard.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/companies-dashboard', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'companies-dashboard', component: CompaniesDashboardComponent },
   {
     path: 'characteristics-dashboard',
